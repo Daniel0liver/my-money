a1b217760ba89010aa72637af661177f21e1a9fa
Daniel Oliveira <dan.oliveira461@gmail.com>
2020-09-04 11:37:18 -0300

    [ADD] Utilizando useReducer e useEffect para buscar dados na Rest Api do firebase e armazenar em um estado

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import axios from 'axios';
 
 import { URL_API } from './services/constants';
 
+// Função pura - Retorna um resultado dado o paramentro
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'LOAD_DATA': {
+      return { ...state, loading: true };
+    }
+    case 'LOAD_DATA_SUCCESS': {
+      return { ...state, loading: false, data: action.data };
+    }
+    default:
+  }
+  return state;
+};
+
 export default function App() {
-  const [data, setData] = useState({});
+  const [data, dispatch] = useReducer(reducer, { loading: true, data: {} });
 
   useEffect(() => {
     axios.get(URL_API).then((response) => {
-      setData(response.data);
+      dispatch({ type: 'LOAD_DATA_SUCCESS', data: response.data });
     });
   }, []);
 
