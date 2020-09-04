0c6eff960bc3fed0556cd38f2526d54c4461315e
Daniel Oliveira <dan.oliveira461@gmail.com>
2020-09-04 10:53:24 -0300

    [ADD] Consumindo rest api do firebase database

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import { URL_API } from './services/constants';
 
 export default function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    axios.get(URL_API).then((response) => {
+      setData(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <p>Hello world</p>
+      <p>My money</p>
+      {JSON.stringify(data)}
     </div>
   );
 }
---
+++ src/services/constants.js
@@ -0,0 +1,2 @@
+export const URL_API =
+  'https://my-money-prime.firebaseio.com/transactions.json';
